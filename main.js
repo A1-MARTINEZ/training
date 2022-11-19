cf4b4396fbfae21f470f5c84c3716378924c41a1
A1-MARTINEZ <116705619+A1-MARTINEZ@users.noreply.github.com>
2022-11-18 16:42:19 -0800

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ const getRandEvent = () => {
 
 // The scope of `days` is too tight 
 const getTrainingDays = event => {
-let days = event;
+let days;
+
   if (event === 'Marathon') {
      days = 50;
   } else if (event === 'Triathlon') {
@@ -26,29 +27,27 @@ let days = event;
 };
 
 // The scope of `name` is too tight 
-const name = 'Nala';
 
-const logEvent = (event) => {
-    
+const logEvent = (name, event) => {
   console.log(`${name}'s event is: ${event}`);
 };
 
-const logTime = (days) => {
-  
+const logTime = (name, days) => {
   console.log(`${name}'s time to train is: ${days} days`);
 };
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+const name = 'Nala';
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+
+
+logEvent(name, event);
+logTime(name, days);
 
 const event2 = getRandEvent();
 const days2 = getTrainingDays(event2);
 const name2 = 'Warren';
- 
+
 logEvent(name2, event2);
 logTime(name2, days2);
-
-const event = getRandEvent();
-const days = getTrainingDays(event);
-// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
-
-
-logEvent(event);
-logTime(days);
